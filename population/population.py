22c9210b2575e50d7bb6408997b3a2f270e3688e
madmoon <madmoontokyo@gmail.com>
2025-09-07 21:36:36 +0700

    nth

--- population/population.py
+++ population/population.py
@@ -1,13 +1,13 @@
-class Population:
-    def __init__(self):
-        pass
+from typing import List
+from ..task.abstract import AbstractTask
 
+class Population:
+    def __init__(self, lst_tasks: List[AbstractTask]):
+        self.lst_tasks = lst_tasks
+        
     def load_pop(self, path:str):
         pass
 
     def save_pop(self, path:str):
         pass
 
-    
-
-    
