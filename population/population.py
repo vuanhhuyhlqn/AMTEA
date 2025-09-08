b067ebbad38ca93cadc5b2c9203af92cd5562c6d
madmoon <madmoontokyo@gmail.com>
2025-09-09 00:08:52 +0700

    harder

--- population/population.py
+++ population/population.py
@@ -21,8 +21,6 @@ class Population:
         for i in range(len(self.lst_tasks)):
             self.lst_taskpopulations.append(TaskPopulation(self.lst_tasks[i], int(self / len(self.lst_tasks))))
         
-
-        
     def load_pop(self, path:str):
         pass
 
--- taskpopulation/taskpopulation.py
+++ taskpopulation/taskpopulation.py
@@ -1,14 +1,31 @@
 from typing import List
 from task import AbstractTask
 from subpopulation import SubPopulation
+from indi import Individual
 from solver import Solver
 
 class TaskPopulation:
-    def __init__(self, task:AbstractTask, size, lst_solvers: List[Solver]):
+    def __init__(self, task : AbstractTask, size : int, lst_solvers: List[Solver]):
         self.task = task
         self.size = size
         self.lst_solvers = lst_solvers
+        self.num_solvers = len(self.lst_solvers)
+        
+
+        self.lst_indis : List[Individual] = []
+        while len(self.lst_indis) < self.size:
+            self.lst_indis.append(Individual(self.task.dim))
 
         
+
+    def remove_solvers(self, solver_ids):
+        pass
+
+    def add_solvers(self, solver_ids):
+        pass
+
+
+        
+
 
         
