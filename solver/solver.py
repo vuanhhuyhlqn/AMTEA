d5d6cc822a36a5529f7e721129a5ece1997affc5
madmoon <madmoontokyo@gmail.com>
2025-09-08 16:15:03 +0700

    simplify solver

--- solver/solver.py
+++ solver/solver.py
@@ -4,9 +4,8 @@ from typing import List
 import numpy as np
 
 class Solver:
-	def __init__(self, num_operands, id):
+	def __init__(self, id):
 		self.id = id
-		self.num_operands = num_operands
 	
 	def __call__(self, operands: List[np.ndarray]) -> np.ndarray:
 		if len(operands) != self.num_operands:
