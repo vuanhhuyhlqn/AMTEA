a11dec02f6f097a24863de91f77bd234efaadcde
madmoon <madmoontokyo@gmail.com>
2025-09-08 15:29:41 +0700

    task name + dimension

--- population/population.py
+++ population/population.py
@@ -2,18 +2,17 @@ from typing import List
 from task import AbstractTask
 from indi import Individual
 from subpopulation import SubPopulation
+import numpy as np
 
 class Population:
-    def __init__(self, lst_tasks: List[AbstractTask], size, load_path=None):
+    def __init__(self, lst_tasks: List[AbstractTask], size):
         self.lst_tasks: List[AbstractTask] = lst_tasks
-        self.lst_indis: List[Individual] = []
+        self.lst_subpopulations: List[SubPopulation] = []
         self.size = size
 
+        for i in range(len(self.lst_tasks)):
+            self.lst_subpopulations.append(SubPopulation(self.lst_tasks[i]))
 
-        if load_path is not None:
-            pass
-        else:
-            pass
         
     def load_pop(self, path:str):
         pass
--- task/abstract.py
+++ task/abstract.py
@@ -1,6 +1,10 @@
 class AbstractTask:
-    def __init__(self, dim):
+    def __init__(self, task_name, dim):
+        self.task_name = task_name
         self.dim = dim
 
     def eval(self, genes):
         pass
+
+    def __str__(self):
+        return f'Task name: {self.task_name}, dimension: {self.dim}'
