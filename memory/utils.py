5c022bd54f2f63a37b3ec9e6da390bd6e6d4f0c5
madmoon <madmoontokyo@gmail.com>
2025-09-09 05:09:36 +0700

    get p value

--- memory/utils.py
+++ memory/utils.py
@@ -1,5 +1,17 @@
+import os
+import json
+
 def get_p_value(task_name: str, solver_id: str) -> float:
-    pass
+    BASE_DIR = os.path.dirname(__file__)
+    file_path = os.path.join(BASE_DIR, "data", solver_id + '.json')
+
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            return data[task_name]
+    else:
+        print('[ERROR] Solver\'s id does not exist')
+        return 0
 
 def update_p_values():
     pass
