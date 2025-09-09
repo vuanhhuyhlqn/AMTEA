51acf6ce1c5f6259e7f17756f7425fbede5e9b53
madmoon <madmoontokyo@gmail.com>
2025-09-10 01:41:34 +0700

    Dict

--- population/population.py
+++ population/population.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 from task import AbstractTask
 from indi import Individual
 from taskpopulation import TaskPopulation
@@ -15,17 +15,20 @@ class Population:
                  initial_lst_solvers: List[Solver],
                  mem: Memory):
         self.lst_tasks: List[AbstractTask] = lst_tasks
-        self.lst_taskpopulations: List[TaskPopulation] = []
+        self.lst_taskpopulations: Dict[str, TaskPopulation] = {}
         self.size = size
         self.num_solvers = num_solvers
         self.lst_solvers = initial_lst_solvers
         self.mem = mem
 
         for i in range(len(self.lst_tasks)):
-            self.lst_taskpopulations.append(TaskPopulation(task=self.lst_tasks[i], 
-                                                           lst_solvers=self.lst_solvers,
-                                                           size=int(self / len(self.lst_tasks)),
-                                                           mem=self.mem))
+            task_name = self.lst_tasks[i].task_name
+            self.lst_taskpopulations[task_name] = TaskPopulation(task=self.lst_tasks[i], 
+                                                                 lst_solvers=self.lst_solvers,
+                                                                 size=int(self / len(self.lst_tasks)),
+                                                                 mem=self.mem)
+            
+        
         
     def load_pop(self, path:str):
         # TODO implement this if have time
