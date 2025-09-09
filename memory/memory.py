6d8f4b6e921cd24c8f4e8036ea8ddb9263324eea
madmoon <madmoontokyo@gmail.com>
2025-09-09 11:49:27 +0700

    more memory

--- memory/memory.py
+++ memory/memory.py
@@ -2,9 +2,35 @@ from typing import List, Dict
 import pandas as pd
 
 class Memory():
-    def __init__(self, memory_size: int):
+    def __init__(self, memory_size: int = 5):
         self.memory_size = memory_size
         self.data = pd.DataFrame(columns=["task_name", "solver_id", "generation", "num_success", "num_failure"])
-        
-
+    
+    def set_value(self, task_name: str, solver_id: str, generation: int, num_success: int, num_failure: int) :
+        mask = (
+            (self.data["task_name"] == task_name) &
+            (self.data["solver_id"] == solver_id) &
+            (self.data["generation"] == generation)
+        )
+        if mask.any():
+            self.data.loc[mask, ["num_success", "num_failure"]] = [num_success, num_failure]
+        else:
+            new_row = {
+                "task_name": task_name,
+                "solver_id": solver_id,
+                "generation": generation,
+                "num_success": num_success,
+                "num_failure": num_failure
+            }
+            self.data = pd.concat([self.data, pd.DataFrame([new_row])], ignore_index=True)
+    
+    def get_value(self, task_name, solver_id, generation):
+        row = self.data[
+            (self.data["task_name"] == task_name) &
+            (self.data["solver_id"] == solver_id) &
+            (self.data["generation"] == generation)
+        ]
+        if row.empty:
+            return None
+        return row[["num_success", "num_failure"]].iloc[0].to_dict()
 
