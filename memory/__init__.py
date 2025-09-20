2c1e11316b61fb9062ad31c6c6a8574cd647c0de
madmoon <madmoontokyo@gmail.com>
2025-09-21 02:47:29 +0700

    razer

--- memory/__init__.py
+++ memory/__init__.py
@@ -1,2 +1,3 @@
 from .utils import *
 from .memory import Memory
+from .record import Record
--- model/amtea.py
+++ model/amtea.py
@@ -9,6 +9,8 @@ from dotenv import load_dotenv
 from matplotlib import pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
+import pandas as pd
+from IPython.display import clear_output
 
 class AMTEA(AbstractModel):
     def __init__(self, pop_size: int, memory_size : int, lst_tasks : List[AbstractTask], model_name = "gpt", num_solvers=2, alpha = 0.2):
@@ -27,6 +29,8 @@ class AMTEA(AbstractModel):
         model = init_llm_model(model_name)
         self.llm = LLM(model)
         
+        self.solver_record : Dict[str, int] = {}
+
         num_llm_solvers = 0
         lst_solvers = []
         ga_solver = Solver('ga', 'Simulated Binary Crossover (SBX) combined with Polynomial Mutation: This operator generates an offspring population by pairing parents from the given population, performing SBX crossover on each pair, and then applying polynomial mutation to introduce additional diversity.', alpha = self.alpha)
@@ -53,7 +57,9 @@ class AMTEA(AbstractModel):
                 lst_solvers.append(solver)
             except Exception as e:
                 print('[ERROR] Create new solver failed!')
-                print(e)
+                # print(e)
+                pass
+
         lst_solvers = sorted(lst_solvers, key=lambda s: s.eval_score, reverse=True)[:num_solvers]
 
         lst_solver_ids = [solver.id for solver in lst_solvers]
@@ -84,7 +90,7 @@ class AMTEA(AbstractModel):
         while self.check_terminate_condition() == False:
             u = self.get_evaluation_count() / eval_budget
             self.alpha = self.alpha_start + (1.0 - self.alpha_start) * (1.0 - math.cos(math.pi * u)) / 2
-            print(f'Alpha: {self.alpha}')
+            # print(f'Alpha: {self.alpha}')
 
             for task_name in self.population.lst_task_names:
                 pop = [indi.gene for indi in self.population.dict_taskpopulations[task_name].lst_indis]
@@ -96,18 +102,51 @@ class AMTEA(AbstractModel):
                     if self.dct_fitness[task_name][-1] > self.dct_fitness[task_name][-2]:
                         raise ValueError("Fitness raise back!")
 
+                if len(self.dct_fitness[task_name]) > 10:
+                    if math.isclose(self.dct_fitness[task_name][-1], self.dct_fitness[task_name][-10], abs_tol=1e-9): # No improvement
+                        ga_solver = Solver('ga', 'Simulated Binary Crossover (SBX) combined with Polynomial Mutation: This operator generates an offspring population by pairing parents from the given population, performing SBX crossover on each pair, and then applying polynomial mutation to introduce additional diversity.', alpha = self.alpha)
+                        de_solver = Solver('de', 'Differential Evolution (DE) Crossover: This operator generates an offspring population by applying DE/rand/1 mutation and binomial crossover to each individual in the given population.', alpha=self.alpha)
+
+                        lst_solvers = [ga_solver, de_solver]
+                        lst_solver_ids = [solver.id for solver in lst_solvers]
+                        self.population.dict_taskpopulations[task_name].num_solvers = len(lst_solvers)
+                        self.population.dict_taskpopulations[task_name].lst_solvers = lst_solvers
+                        self.population.dict_taskpopulations[task_name].mem.restart(lst_solver_ids)
+                        gen = 0
+
             gen += 1
 
             self.population.evolve(gen=gen, lp=lp, tgap=tgap, k=k)
-            
+
             if monitor == True and gen % monitor_rate == 0:
-                print(f'Generation {gen}:')
-                print(self.population.dict_best_fitness)
+                # print(f'Generation {gen}:')
+                # print(self.population.dict_best_fitness)
+
+                line = f'Evaluation count : {self.get_evaluation_count()} / {eval_budget} - Obj: '
+                for task_name, best_fitness in self.population.dict_best_fitness.items():
+                    line += f'{best_fitness:.5f} '
+                for task_name in self.population.lst_task_names:
+                    line += f'{[solver.id for solver in self.population.dict_taskpopulations[task_name].lst_solvers]} '
+
+                if gen % up == 0:
+                    line += '[*]'
+                else:
+                    line += '[+]'
+                
+                clear_output(wait=True)
+                print("\r" + line)
 
             # Update solvers
             if gen % up == 0:
                 self.update_solvers()
                 gen = 0
+
+        lst_record_dfs : List[pd.DataFrame] = []
+        for task_name in self.population.lst_task_names:
+            lst_record_dfs.append(self.population.dict_taskpopulations[task_name].record.data)
+        
+        merged_record_df = pd.concat(lst_record_dfs).groupby("solver_id", as_index=False)["evaluation_count"].sum()
+        print(merged_record_df)
 
         if delete_after_run: # Delete all solvers in cached folder after run
             delete_all()
@@ -121,7 +160,7 @@ class AMTEA(AbstractModel):
 
     def update_solvers(self):
         for task_name in self.population.lst_task_names:
-            print(f'Updating solvers for task {task_name} ... ')
+            # print(f'Updating solvers for task {task_name} ... ')
             lst_solvers = self.population.dict_taskpopulations[task_name].lst_solvers
             mem = self.population.dict_taskpopulations[task_name].mem
             best_solver_id = mem.get_best_solver_id()
@@ -142,15 +181,15 @@ class AMTEA(AbstractModel):
             # Create new solver
             lst_indis = self.population.dict_taskpopulations[task_name].lst_indis
             eval_check_score = worst_solver.evaluate_task(lst_indis, self.alpha)
-            print(f'[*] Evaluation score threshold for new solver: {eval_check_score * 0.7:.5f}')
+            # print(f'[*] Evaluation score threshold for new solver: {eval_check_score * 0.9:.5f}')
             eval_check_count = 0
             
             for solver in lst_solvers:
                 solver.eval_score = solver.evaluate_task(lst_indis, self.alpha)
-                if solver.eval_score >= (eval_check_score * 0.7):
+                if solver.eval_score >= (eval_check_score * 0.9):
                     eval_check_count += 1
-                print(f'EVAL CHECK COUNT 1: {eval_check_count}')
-                print(f'Solver {solver.id}, eval_score: {solver.eval_score:.5f}')
+                # print(f'EVAL CHECK COUNT 1: {eval_check_count}')
+                # print(f'Solver {solver.id}, eval_score: {solver.eval_score:.5f}')
                 
             num_llm_solvers = 5
             num_try = 0
@@ -158,19 +197,19 @@ class AMTEA(AbstractModel):
                 num_try += 1
                 if num_try > 10:
                     break
-
                 try:
                     [id, alg] = self.llm.update_solver(good_solvers_history, worst_solvers_history, self.alpha)
                     solver = Solver(id, alg)
                     solver.eval_score = solver.evaluate_task(lst_indis, self.alpha) 
-                    print(f'LLM Solver {len(lst_solvers) - self.num_solvers + 2}: {solver.id}, eval_score: {solver.eval_score:.5f}')
+                    # print(f'LLM Solver {len(lst_solvers) - self.num_solvers + 2}: {solver.id}, eval_score: {solver.eval_score:.5f}')
                     lst_solvers.append(solver)
-                    if solver.eval_score >= (eval_check_score * 0.7):
+                    if solver.eval_score >= (eval_check_score * 0.9):
                         eval_check_count += 1
-                    print(f'EVAL CHECK COUNT 2: {eval_check_count}')
+                    # print(f'EVAL CHECK COUNT 2: {eval_check_count}')
                 except Exception as e:
-                    print('[ERROR] Create new solver failed!')
-                    print(e)
+                    # print('[ERROR] Create new solver failed!')
+                    # print(e)
+                    pass 
 
             lst_solvers = sorted(lst_solvers, key=lambda s: s.eval_score, reverse=True)[:self.num_solvers]
             self.population.dict_taskpopulations[task_name].lst_solvers = lst_solvers
@@ -180,7 +219,7 @@ class AMTEA(AbstractModel):
         eval_cnt = 0
         for task in self.lst_tasks:
             eval_cnt += task.eval_cnt
-        print(f'Evaluation count: {eval_cnt}/{self.eval_budget}')
+        # print(f'Evaluation count: {eval_cnt}/{self.eval_budget}')
         return eval_cnt >= self.eval_budget
 
     def get_evaluation_count(self) -> int:
